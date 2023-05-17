f1ad9610818376c145c87056f9260433e8e8faa9
Satyajit Sarangi <satyajit.sarangi@gmail.com>
2023-05-17 10:41:55 -0700

    Minor formatting fixes.

--- src/pages/views/DesignEditor/components/Canvas/Canvas.tsx
+++ src/pages/views/DesignEditor/components/Canvas/Canvas.tsx
@@ -1,31 +1,31 @@
-import React from "react"
-import { Canvas as LayerhubCanvas } from "@layerhub-io/react"
-import Playback from "../Playback"
-import useDesignEditorContext from "~/hooks/useDesignEditorContext"
-import ContextMenu from "../ContextMenu"
+import React from 'react';
+import {Canvas as LayerhubCanvas} from '@layerhub-io/react';
+import Playback from '../Playback';
+import useDesignEditorContext from '~/hooks/useDesignEditorContext';
+import ContextMenu from '../ContextMenu';
 
 const Canvas = () => {
-  const { displayPlayback } = useDesignEditorContext()
+  const {displayPlayback} = useDesignEditorContext();
   return (
-    <div style={{ flex: 1, display: "flex", position: "relative" }}>
+    <div style={{flex: 1, display: 'flex', position: 'relative'}}>
       {displayPlayback && <Playback />}
       <ContextMenu />
       <LayerhubCanvas
         config={{
-          background: "#f1f2f6",
+          background: '#f1f2f6',
           controlsPosition: {
-            rotation: "BOTTOM",
+            rotation: 'BOTTOM',
           },
           shadow: {
             blur: 4,
-            color: "#fcfcfc",
+            color: '#fcfcfc',
             offsetX: 0,
             offsetY: 0,
           },
         }}
       />
     </div>
-  )
-}
+  );
+};
 
-export default Canvas
+export default Canvas;
