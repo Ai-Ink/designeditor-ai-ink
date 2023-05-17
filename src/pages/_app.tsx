87855b9cc282ec88bfc208c50d1489e3fed02113
Satyajit Sarangi <satyajit.sarangi@gmail.com>
2023-05-17 09:59:10 -0700

    Initial Commit with baseweb and pixi.js

---
+++ src/pages/_app.tsx
@@ -0,0 +1,20 @@
+import ReactDOM from 'react-dom/client';
+import Provider from './Provider';
+import Router from './Router';
+import Container from './Container';
+import './styles/styles.css';
+import {useEffect, useState} from 'react';
+
+function MyApp({Component, pageProps}) {
+  const [render, setRender] = useState(false);
+  useEffect(() => setRender(true), []);
+  return render ? (
+    <Provider>
+      <Container>
+        <Router />
+      </Container>
+    </Provider>
+  ) : null;
+}
+
+export default MyApp;
