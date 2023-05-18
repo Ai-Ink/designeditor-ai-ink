3714766d491b93c3b642d40ef98be6ffa5afd112
Satyajit Sarangi <satyajit.sarangi@gmail.com>
2023-05-17 21:05:31 -0700

    Initial commit for Chakra UI migration

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,16 +4,33 @@ import Router from './Router';
 import Container from './Container';
 import './styles/styles.css';
 import {useEffect, useState} from 'react';
+import {ChakraProvider} from '@chakra-ui/react';
+
+// 1. Import the extendTheme function
+import {extendTheme} from '@chakra-ui/react';
+
+// 2. Extend the theme to include custom colors, fonts, etc
+const colors = {
+  brand: {
+    900: '#1a365d',
+    800: '#153e75',
+    700: '#2a69ac',
+  },
+};
+
+export const theme = extendTheme({colors});
 
 function MyApp({Component, pageProps}) {
   const [render, setRender] = useState(false);
   useEffect(() => setRender(true), []);
   return render ? (
-    <Provider>
-      <Container>
-        <Router />
-      </Container>
-    </Provider>
+    <ChakraProvider theme={theme}>
+      <Provider>
+        <Container>
+          <Router />
+        </Container>
+      </Provider>
+    </ChakraProvider>
   ) : null;
 }
 
